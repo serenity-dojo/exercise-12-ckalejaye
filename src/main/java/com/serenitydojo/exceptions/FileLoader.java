845cb253ca16d0e601b49823e821906e0f6cfdd6
Cynthia Kalejaye <ckalejaye@gmail.com>
2022-11-17 09:52:39 +0000

    Exercise 12 current state

--- src/main/java/com/serenitydojo/exceptions/FileLoader.java
+++ src/main/java/com/serenitydojo/exceptions/FileLoader.java
@@ -2,28 +2,50 @@ package com.serenitydojo.exceptions;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 
+import static java.nio.file.Files.*;
+
 public class FileLoader {
-    public String readHelloWorld() throws IOException {
-        return Files.readString(Paths.get("src/main/resources/hello.txt"));
-    }
+
+       public static String readHelloWorld(String filename) throws IOException {
+       String fileContents = Files.readString(Paths.get(filename));
+         return readHelloWorld(fileContents);
+
+      }
 
     public boolean fileContainsText(String filename, String expectedText) {
-        String path = "src/main/resources/" + filename;
+
         try {
-            return (Files.readString(Paths.get(path)).contains(expectedText));
+ //           String path = "src/main/resources/" + filename;
+            String fileContents = Files.readString(Paths.get(filename));
+            return (readString(Paths.get(filename)).contains(expectedText));
         } catch (IOException e) {
+            return true;
+        }
+    }
+
+
+    public boolean fileDoesNotContainsText(String filename, String expectedText) {
+
+        try {
+            //           String path = "src/main/resources/" + filename;
+            String fileContents = Files.readString(Paths.get(filename));
+            return (readString(Paths.get(filename)).contains(expectedText));
+        } catch (NoSuchFileException e) {
             return false;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public boolean fileHasText(String filename, String expectedText) {
         String path = "src/main/resources/" + filename;
         try {
-            return (Files.readString(Paths.get(path)).contains(expectedText));
+            return (readString(Paths.get(path)).contains(expectedText));
         } catch (IOException e) {
             throw new MissingWelcomeFileException("Missing welcome file: " + filename, e);
         }
     }
-}
+ }
